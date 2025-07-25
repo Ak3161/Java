e54972ed49a4d0cc833260e8da1d7670820754d1
Aniket Kumawat <121565314+Ak3161@users.noreply.github.com>
2025-07-25 18:50:15 +0530

    RemoveConsecutiveCharacters.java

---
+++ String/Upgrade to Premium for Doubt Support across all your problems and courses.  Explore Premium Redirection Icon RemoveConsecutiveCharacters.java
@@ -0,0 +1,18 @@
+
+
+class Solution {
+    public String removeConsecutiveCharacter(String s) {
+        int n = s.length();
+        StringBuilder res = new StringBuilder();
+        
+        for(int i = 0; i < n; i++){
+            if(i < n-1 && s.charAt(i) == s.charAt(i+1)){
+                continue;
+            }   else{
+                res.append(s.charAt(i));
+            }
+        }
+        return res.toString();
+        
+    }
+}
